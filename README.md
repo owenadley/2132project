8e149dd0d8f9d70a97e20f1ed798d4542e6ad2ea
Owen Adley <oadle049@uottawa.ca>
2018-03-22 18:32:12 +0000

    qt 4 - working connection & tables

--- README.md
+++ README.md
@@ -7,23 +7,9 @@
      `-----'`--' `---'  `----'  `---'  `--'    `--'`-------' `------'
     ----------------------------------------------------------------- 
 
-
-Hi there! Welcome to Cloud9 IDE!
-
-To get you started, we have created a small hello world application.
-
-1) Open the hello-world.php file
-
-2) Follow the run instructions in the file's comments
-
-3) If you want to look at the Apache logs, check out ~/lib/apache2/log
-
-And that's all there is to it! Just have fun. Go ahead and edit the code, 
-or add new files. It's all up to you! 
-
-Happy coding!
-The Cloud9 IDE team
-
 # 2132project
+
 Databases 1 Final Project
->>>>>>> 0b9c40101b065e0c260414e9ca103c86f4cfc81c
+
+Utilizing PostgreSQL db to create an application which records ratings and menus of various resturaunts and allows for advanced queries of data.
+
--- index.php
+++ index.php
@@ -1,20 +1,18 @@
 <html><body>
 <?php
-// A simple web site in Cloud9 that runs through Apache
-// Press the 'Run' button on the top to start the web server,
-// then click the URL that emitted to the Output tab of the console
 
-echo 'Hello world from Cloud9!';
+echo "Hello world from Cloud9! \n";
 
+#==================== CONNECTION ========================
+# Extract connection from db URL
 function pg_connection_string_from_database_url() {
   extract(parse_url($_ENV["postgres://xhgocwtwpvuyuh:1c568d1c618b10132266a428b65cc08dcb75ea25e71697aaada2da222231dae5@ec2-54-243-210-70.compute-1.amazonaws.com:5432/d88e4kacmh5m8a
 "]));
-  return "user=xhgocwtwpvuyuh password=1c568d1c618b10132266a428b65cc08dcb75ea25e71697aaada2da222231dae5 host=ec2-54-243-210-70.compute-1.amazonaws.com dbname=d88e4kacmh5m8a" . substr($path, 1); # <- you may want to add sslmode=require there too
+  return "user=xhgocwtwpvuyuh password=1c568d1c618b10132266a428b65cc08dcb75ea25e71697aaada2da222231dae5 host=ec2-54-243-210-70.compute-1.amazonaws.com dbname=d88e4kacmh5m8a" . substr($path, 1);
 }
 
-# Here we establish the connection. Yes, that's all.
+# Establish connection
 $conn = pg_connect(pg_connection_string_from_database_url());
-
 
 if (!$conn) {
   echo "An error occurred.\n";
@@ -23,19 +21,20 @@ if (!$conn) {
     echo 'Connected';
 }
 
-# Now let's use the connection for something silly just to prove it works:
+# Testing connection
 $result = pg_query($conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
 print "<pre>\n";
 if (!pg_num_rows($result)) {
-  print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
+  print("Connection is working, but database is empty.\n");
 } else {
   print "Tables in your database:\n";
   while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
 }
+#==================== / CONNECTION ========================
 
 
 $test = pg_query($conn, "CREATE TABLE food (name varchar(255))");
-$test2 = pg_query($conn, "INSERT INTO food (name) VALUES (pizza)");
+$test2 = pg_query($conn, "INSERT INTO food (name) VALUES ('pizza')");
 
 $result = pg_query($conn, "SELECT name FROM food");
 if (!$result) {
@@ -46,6 +45,8 @@ if (!$result) {
 while ($row = pg_fetch_row($result)) {
   echo "Food: $row[0]";
   echo "<br />\n";
+} else {
+  echo 'No records in food';
 }
 
 ?>
