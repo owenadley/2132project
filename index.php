03cff6d143051aaa58ac5b7bf0320f29edf85f53
Owen Adley <owen.adley@gmail.com>
2018-04-05 14:54:50 +0000

    queries test

--- index.php
+++ index.php
@@ -554,13 +554,7 @@ while ($row = pg_fetch_assoc($result)) {
 $userIDSelect = "voldy";
 
 $result = pg_query($conn, 
-"SELECT R.Name, RL.firstOpenDate
-FROM Restaurant R, Location RL
-WHERE Rating 
-AND R.RestaurantID = (SELECT RestaurantID FROM Rating Ra WHERE ($userIDSelect. >  *.Staff )) 
-ORDER BY RL.firstOpenDate, R.name ASC");
-
-SELECT Rest.Name, RL.firstOpenDate
+"SELECT Rest.Name, RL.firstOpenDate
 FROM Rating Rat
 LEFT JOIN Restaurant Rest ON Rat.RestaurantID=Rest.RestaurantID
 LEFT JOIN Location RL ON RL.RestaurantID=Rat.RestaurantID
@@ -576,7 +570,7 @@ WHERE $userIDSelect=Rat.UserID
         WHERE Rat.RestaurantID=Rest.RestaurantID)
       AND Rat.Staff>( SELECT AVG(Rat.Staff)
         FROM Rating Rat, Restaurant Rest
-        WHERE Rat.RestaurantID=Rest.RestaurantID))
+        WHERE Rat.RestaurantID=Rest.RestaurantID))");
 
 
 
