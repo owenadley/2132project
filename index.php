3821d41c35ff6773b14801d31ee9229cd16d55a4
Owen Adley <owen.adley@gmail.com>
2018-04-05 15:50:03 +0000

    queries test

--- index.php
+++ index.php
@@ -616,7 +616,7 @@ $result = pg_query($conn,
 "SELECT R.name
 FROM Restaurant R, Rating Ra
 WHERE 
-(SELECT AVG(Ra.price + Ra.food + Ra.mood) / 3 FROM Rating Ra AS AverageRating 
+(SELECT AVG(Ra.price + Ra.food + Ra.mood) / 3 FROM Rating Ra
 WHERE Ra.RestaurantID = (SELECT R.RestaurantID FROM Restaurant R WHERE R.syle = '$typeSelect')) ");
 
 if (!$result) {
