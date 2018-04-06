3946fc2b57c1b38e8e1f7dc7e4a64c9f06280246
Owen Adley <paliwal.anushka@gmail.com>
2018-04-06 18:56:25 +0000

    testing queries

--- index.php
+++ index.php
@@ -653,7 +653,7 @@ $result = pg_query($conn,
 FROM Rater Ra, Restaurant R, Rating Rat
 WHERE R.RestaurantID = Rat.restaurantID
       AND Ra.userID = Rat.userID
-      AND Ra.userID = (SELECT Rat.userID FROM Rating Rat 
+      AND Ra.userID = (SELECT DISTINCT Rat.userID FROM Rating Rat 
                       WHERE ((SELECT (Rat.Food+Rat.Mood) 
                               FROM Rating Rat 
                               LEFT JOIN Rating Ra ON Ra.userID=Rat.userID) 
