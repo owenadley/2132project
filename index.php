e64e9e9006f8f3c2458975f9f345fa0acef3faff
Owen Adley <paliwal.anushka@gmail.com>
2018-04-06 03:41:12 +0000

    Testing queries

--- index.php
+++ index.php
@@ -582,8 +582,8 @@ if (!$result) {
 
 while ($row = pg_fetch_assoc($result)) {
   echo " My query: \n";
-  echo " $row[firstOpenDate] \n";
-  echo " $row[name] \n";
+  echo " $row[1] \n";
+  echo " $row[0] \n";
 }
 
 
