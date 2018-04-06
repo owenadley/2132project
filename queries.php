3849612d23b234e9224088b2d7ca385404845ddc
Owen Adley <paliwal.anushka@gmail.com>
2018-04-06 19:43:07 +0000

    front end

---
+++ queries.php
@@ -0,0 +1,131 @@
+<html><body>
+    
+<head>
+  <meta charset="UTF-8">
+  <meta author="Anushka Paliwal, Owen Adley">
+  <title>db2132 Project</title>
+  
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="frontend/style.css">  
+</head>
+    
+<div class='header'>
+  <h3 id='headerTitle'>2132 Foods</h3>
+</div>
+    
+<div class='core-content'>
+  
+  <br>
+  
+  <div class='row'>
+   
+    <div class='col-md-3'>
+      
+      <div class='sidemenu'>
+        <div class='sidemenu-option'>
+          <p class='nopad sidemenup'>Home</p>
+        </div>
+        <div class='sidemenu-option'>
+          <p class='nopad sidemenup'>Resturaunts Nearby</p>
+        </div>
+        <div class='sidemenu-option'>
+          <p class='nopad sidemenup'>Browse Resturaunts</p>
+        </div>
+        <div class='sidemenu-option'>
+          <p class='nopad sidemenup'>Resturaunt Queries</p>
+        </div>
+      </div>
+      
+    </div>
+    <div class='col-md-9'>
+      <div class='core-panels'>
+        <div class='row'>
+          <div class='col-md-3'>
+            <div class='panel'>
+              <p>Query 1a</p>
+            </div>
+          </div>
+          <div class='col-md-3'>
+            <div class='panel'>
+              <p>Query 1b</p>
+            </div>
+          </div> 
+          <div class='col-md-3'>
+            <div class='panel'>
+              <p>Query 1c</p>
+            </div>
+          </div> 
+          <div class='col-md-3'>
+            <div class='panel'>
+              <p>Query 1d</p>
+            </div>
+          </div> 
+          <div class='col-md-3'>
+            <div class='panel'>
+              <p>Query 1e</p>
+            </div>
+          </div> 
+          <div class='col-md-3'>
+            <div class='panel'>
+              <p>Query 2f</p>
+            </div>
+          </div> 
+          <div class='col-md-3'>
+            <div class='panel'>
+              <p>Query 2g</p>
+            </div>
+          </div> 
+          <div class='col-md-3'>
+            <div class='panel'>
+              <p>Query 2h</p>
+            </div>
+          </div> 
+          <div class='col-md-3'>
+            <div class='panel'>
+              <p>Query 2i</p>
+            </div>
+          </div>
+          <div class='col-md-3'>
+            <div class='panel'>
+              <p>Query 2j</p>
+            </div>
+          </div>
+          <div class='col-md-3'>
+            <div class='panel'>
+              <p>Query 3k</p>
+            </div>
+          </div>
+          <div class='col-md-3'>
+            <div class='panel'>
+              <p>Query 3l</p>
+            </div>
+          </div>
+          <div class='col-md-3'>
+            <div class='panel'>
+              <p>Query 3m</p>
+            </div>
+          </div>
+          <div class='col-md-3'>
+            <div class='panel'>
+              <p>Query 3n</p>
+            </div>
+          </div>
+          <div class='col-md-3'>
+            <div class='panel'>
+              <p>Query 3o</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  
+</div>
+    
+    
+    
+    
+    
+</body></html>
