1b49519fd9074780e79595081fdf4cda0076ca4a
Owen Adley <paliwal.anushka@gmail.com>
2018-04-06 19:51:32 +0000

    front end

--- index.php
+++ index.php
@@ -58,16 +58,16 @@ if (!pg_num_rows($result)) {
       
       <div class='sidemenu'>
         <div class='sidemenu-option'>
-          <p class='nopad sidemenup'>Home</p>
+          <a href='index.php'><p class='nopad sidemenup'>Home</p></a>
         </div>
         <div class='sidemenu-option'>
-          <a href='queries.php'><p class='nopad sidemenup'>Resturaunts Nearby</p></a>
+          <a href='resturaunts.php'><p class='nopad sidemenup'>Browse Resturaunts</p></a>
         </div>
         <div class='sidemenu-option'>
-          <p class='nopad sidemenup'>Browse Resturaunts</p>
+          <a href='userResturaunts.php'><p class='nopad sidemenup'>My Resturaunts</p></a>
         </div>
         <div class='sidemenu-option'>
-          <p class='nopad sidemenup'>Resturaunt Queries</p>
+          <a href='queries.php'><p class='nopad sidemenup'>Resturaunt Queries</p></a>
         </div>
       </div>
       
