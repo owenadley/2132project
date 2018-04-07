92142c95f60117a8bb07d2222a6beca95eba866e
Owen Adley <paliwal.anushka@gmail.com>
2018-04-07 00:52:10 +0000

    front end

---
+++ Resturaunts.php
@@ -0,0 +1,50 @@
+<html><body>
+    
+<head>
+  <meta charset="UTF-8">
+  <meta author="Anushka Paliwal, Owen Adley">
+  <title>db2132 Project</title>
+  
+  <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="frontend/style.css">  
+</head>
+    
+<div class='header'>
+  <h3 id='headerTitle'>2132 Foods</h3>
+</div>
+
+<div class='core-content'>
+  
+  <br>
+  
+  <div class='row'>
+   
+    <div class='col-md-3'>
+      
+      <div class='sidemenu'>
+        <div class='sidemenu-option'>
+          <a href='index.php'><p class='nopad sidemenup'>Home</p></a>
+        </div>
+        <div class='sidemenu-option'>
+          <a href='resturaunts.php'><p class='nopad sidemenup'>Browse Resturaunts</p></a>
+        </div>
+        <div class='sidemenu-option'>
+          <a href='userResturaunts.php'><p class='nopad sidemenup'>My Resturaunts</p></a>
+        </div>
+        <div class='sidemenu-option'>
+          <a href='queries.php'><p class='nopad sidemenup'>Resturaunt Queries</p></a>
+        </div>
+      </div>
+      
+    </div>
+    <div class='col-md-9'>
+      <div class='core-userResturaunts.php'>
+        <h2>Browse Resturaunts</h2>
+
+      </div>
+    </div>
+  </div>
+</div>
+
+</body></html>
