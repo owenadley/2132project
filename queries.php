df93be372ff8209c1b603d3783a004aeed0d07e6
Owen Adley <owen.adley@gmail.com>
2018-04-08 21:55:39 +0000

    user resturaunts

--- queries.php
+++ queries.php
@@ -200,53 +200,96 @@ location tables should then displayed on the screen.
               <?php endwhile; ?>
             </table>
           </div>
-
       </div>
+      
       <div id='queryDisplay1b' class='queryDisplay'>
         <div class='queryExitIcon'><i class="fas fa-times exitQuery" onclick='hideQuery1b()'></i></div>
-        b : Display all the information about a user‐specified restaurant. That is, the user should select the
-name of the restaurant from a list, and the information as contained in the restaurant and
-location tables should then displayed on the screen.
+        b : Display the full menu of a specific restaurant. That is, the user should select the name of the
+restaurant from a list, and all menu items, together with their prices, should be displayed on the
+screen. The menu should be displayed based on menu item categories.
+          <?php 
+            $resturauntselect = "Wendys";
+            $result = pg_query($conn, "SELECT M.* FROM MenuItem M, Restaurant R WHERE R.name = '$resturauntselect' AND M.restaurantID = R.RestaurantID");
+              
+            if (!$result) {
+            echo "An error occurred.\n";
+            exit;
+            }
+          ?>
+            
+            <div class="container">
+              <br/>
+              <table>
+                <tr>
+                  <th>Item ID</th>
+                  <th>Name</th>
+                  <th>Type</th>
+                  <th>Category</th>
+                  <th>Description ID</th>
+                  <th>Price</th>
+                  <th>Restaurant ID</th>
+                </tr>
+                <?php while ($row = pg_fetch_assoc($result)): ?>
+                <tr>
+                  <td><?php echo $row['ItemID']; ?></td>
+                  <td><?php echo $row['name']; ?></td>
+                  <td><?php echo $row['type']; ?></td>
+                  <td><?php echo $row['category']; ?></td>
+                  <td><?php echo $row['description']; ?></td>
+                  <td><?php echo $row['price']; ?></td>
+                  <td><?php echo $row['RestaurantID']; ?></td>
+                </tr>
+                <?php endwhile; ?>
+              </table>
+            </div>
       </div> 
+      
       <div id='queryDisplay1c' class='queryDisplay'>
         <div class='queryExitIcon'><i class="fas fa-times exitQuery" onclick='hideQuery1c()'></i></div>
         c : For each user‐specified category of restaurant, list the manager names together with the date
 that the locations have opened. The user should be able to select the category (e.g. Italian or
 Thai) from a list.
       </div>
+      
       <div id='queryDisplay1d' class='queryDisplay'>
         <div class='queryExitIcon'><i class="fas fa-times exitQuery" onclick='hideQuery1d()'></i></div>
         d : Given a user‐specified restaurant, find the name of the most expensive menu item. List this
 information together with the name of manager, the opening hours, and the URL of the
 restaurant. The user should be able to select the restaurant name (e.g. El Camino) from a list.
       </div>
+      
       <div id='queryDisplay1e' class='queryDisplay'>
         <div class='queryExitIcon'><i class="fas fa-times exitQuery" onclick='hideQuery1e()'></i></div>
         e : For each type of restaurant (e.g. Indian or Irish) and the category of menu item (appetiser, main
 or desert), list the average prices of menu items for each category.
       </div>
       
+      
       <div id='queryDisplay2f' class='queryDisplay'>
         <div class='queryExitIcon'><i class="fas fa-times exitQuery" onclick='hideQuery2f()'></i></div>
         f : Find the total number of rating for each restaurant, for each rater. That is, the data should be
 grouped by the restaurant, the specific raters and the numeric ratings they have received.
       </div>
+      
       <div id='queryDisplay2g' class='queryDisplay'>
         <div class='queryExitIcon'><i class="fas fa-times exitQuery" onclick='hideQuery2g()'></i></div>
         g : Display the details of the restaurants that have not been rated in January 2015. That is, you should display the name of the restaurant together with the phone number and the type of
 food.
       </div> 
+      
       <div id='queryDisplay2h' class='queryDisplay'>
         <div class='queryExitIcon'><i class="fas fa-times exitQuery" onclick='hideQuery2h()'></i></div>
         h : Find the names and opening dates of the restaurants that obtained Staff rating that is lower
 than any rating given by rater X. Order your results by the dates of the ratings. (Here, X refers to
 any rater of your choice.)
       </div>
+      
       <div id='queryDisplay2i' class='queryDisplay'>
         <div class='queryExitIcon'><i class="fas fa-times exitQuery" onclick='hideQuery2i()'></i></div>
         i : List the details of the Type Y restaurants that obtained the highest Food rating. Display the
 restaurant name together with the name(s) of the rater(s) who gave these ratings. (Here, Type Y refers to any restaurant type of your choice, e.g. Indian or Burger.)
       </div>
+      
       <div id='queryDisplay2j' class='queryDisplay'>
         <div class='queryExitIcon'><i class="fas fa-times exitQuery" onclick='hideQuery2j()'></i></div>
         j : Provide a query to determine whether Type Y restaurants are “more popular” than other restaurants. (Here, Type Y refers to any restaurant type of your choice, e.g. Indian or Burger.) Yes, this query is open to your own interpretation!
@@ -259,21 +302,25 @@ restaurant name together with the name(s) of the rater(s) who gave these ratings
 terms of the Food and the Mood of restaurants. Display this information together with the
 names of the restaurant and the dates the ratings were done.
       </div>
+      
       <div id='queryDisplay3l' class='queryDisplay'>
         <div class='queryExitIcon'><i class="fas fa-times exitQuery" onclick='hideQuery3l()'></i></div>
         l : Find the names and reputations of the raters that give the highest overall rating, in terms of the
 Food or the Mood of restaurants. Display this information together with the names of the
 restaurant and the dates the ratings were done.
       </div> 
+      
       <div id='queryDisplay3m' class='queryDisplay'>
         <div class='queryExitIcon'><i class="fas fa-times exitQuery" onclick='hideQuery3m()'></i></div>
         m : Find the names and reputations of the raters that rated a specific restaurant (say Restaurant Z)
 the most frequently. Display this information together with their comments and the names and prices of the menu items they discuss. (Here Restaurant Z refers to a restaurant of your own choice, e.g. Ma Cuisine).
       </div>
+      
       <div id='queryDisplay3n' class='queryDisplay'>
         <div class='queryExitIcon'><i class="fas fa-times exitQuery" onclick='hideQuery3n()'></i></div>
         n : Find the names and emails of all raters who gave ratings that are lower than that of a rater with a name called John, in terms of the combined rating of Price, Food, Mood and Staff. (Note that there may be more than one rater with this name).
       </div>
+      
       <div id='queryDisplay3o' class='queryDisplay'>
         <div class='queryExitIcon'><i class="fas fa-times exitQuery" onclick='hideQuery3o()'></i></div> 
         o : Find the names, types and emails of the raters that provide the most diverse ratings. Display this information together with the restaurants names and the ratings. For example, Jane Doe may have rated the Food at the Imperial Palace restaurant as a 1 on 1 January 2015, as a 5 on 15 January 2015, and a 3 on 4 February 2015. Clearly, she changes her mind quite often.
