44de9e5c72158ad1907487da8802d9f64cbbfce3
Owen Adley <owen.adley@gmail.com>
2018-04-09 15:17:40 +0000

    login return requests

--- queries.php
+++ queries.php
@@ -138,9 +138,9 @@ $conn = pg_connect(pg_connection_string_from_database_url());
  												<input id='send_updates' type='checkbox' />
  												<label for='send_updates'>Send me occasional email updates</label>
  										</div>
- 
-                    <input type='hidden' name='returnAddr' value='queries.php'>
-                    
+                     
+                    <input type='hidden' name='returnAddr' value='queries.php' required/>
+                    <?phpecho"lol";?>
  										<div class='action_btns'>
  												<div class='one_half'><a onclick='back();' class='btn back_btn'><i class='fa fa-angle-double-left'></i> Back</a></div>
  												<div class='one_half last'><input type='submit' class='btn btn_red' value='Register'></input></div>
