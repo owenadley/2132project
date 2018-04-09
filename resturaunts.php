4b671e4de41cdb93e6423caa11a84c15334d9f52
Owen Adley <owen.adley@gmail.com>
2018-04-09 04:12:09 +0000

    adding menuitem

--- resturaunts.php
+++ resturaunts.php
@@ -1,5 +1,39 @@
 <html><body>
-    
+<?php
+
+session_start();
+
+#==================== CONNECTION =========================
+# Extract connection from db URL
+function pg_connection_string_from_database_url() {
+  extract(parse_url($_ENV["postgres://xhgocwtwpvuyuh:1c568d1c618b10132266a428b65cc08dcb75ea25e71697aaada2da222231dae5@ec2-54-243-210-70.compute-1.amazonaws.com:5432/d88e4kacmh5m8a
+"]));
+  return "user=xhgocwtwpvuyuh password=1c568d1c618b10132266a428b65cc08dcb75ea25e71697aaada2da222231dae5 host=ec2-54-243-210-70.compute-1.amazonaws.com dbname=d88e4kacmh5m8a" . substr($path, 1);
+}
+
+# Establish connection
+$conn = pg_connect(pg_connection_string_from_database_url());
+
+if (!$conn) {
+  echo "An error occurred.\n";
+  exit;
+} else {
+    #echo 'Connected';
+}
+
+# Testing connection
+/*$result = pg_query($conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
+print "<pre>\n";
+if (!pg_num_rows($result)) {
+  print("Connection is working, but database is empty.\n");
+} else {
+  print "Tables in your database:\n";
+  while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
+}*/
+#==================== / CONNECTION =======================
+
+?>
+
 <head>
   <meta charset="UTF-8">
   <meta author="Anushka Paliwal, Owen Adley">
@@ -64,7 +98,7 @@
 				if ($_POST['type'] != null) {
 				  $type = $_POST['type'];
 				  echo $_POST['type'];
-				  $filterType = pg_query($conn, "SELECT * FROM Restaurant");
+				  $filterType = pg_query($conn, "SELECT * FROM Restaurant WHERE Type='$type'");
 				  if (!$filterType) {
 				    echo "err";
 				  }
