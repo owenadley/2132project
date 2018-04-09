bbfbe3a3a36a86f606892bb7e463a4db2a67d30a
Owen Adley <owen.adley@gmail.com>
2018-04-09 16:33:13 +0000

    login return requests

--- queries.php
+++ queries.php
@@ -943,7 +943,7 @@ the most frequently. Display this information together with their comments and t
               WHERE  ((SELECT SUM((Rat.Price)+(Rat.Food)+(Rat.Mood)+(Rat.Staff)) AS total
                                               FROM Rating Rat, Rater R 
                                               WHERE Rat.UserID=R.UserID 
-                                              AND total <
+                                              AND ((SELECT SUM((Rat.Price)+(Rat.Food)+(Rat.Mood)+(Rat.Staff)) <
                                                                                       
                                               (SELECT DISTINCT SUM((Ra.Price)+(Ra.Food)+(Ra.Mood)+(Ra.Staff)) AS tots
                                                 FROM Rating Ra, Rater Ru WHERE Ra.UserID = 'John')))
